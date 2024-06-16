f00f4d2a8cc1470743b7c9129067778092b6beb3
wonseokyoon <01088115030a@gmail.com>
2024-06-16 15:54:23 +0900

    debuging

--- src/main/java/com/example/todo/controller/AuthController.java
+++ src/main/java/com/example/todo/controller/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins ="http://localhost:3000")
 public class AuthController {
 
     @Autowired
